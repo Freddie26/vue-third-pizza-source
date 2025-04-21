44ee5858295fc85f0d739448d6ec31e46a0ad11b
ianatskiy <igor.anatsky@waveaccess.ru>
2025-04-21 19:08:52 +0300

    section-0: Первые шаги

---
+++ course/section0/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppHeader from "./AppHeader.vue";
+
+</script>
+
+<template>
+  <div>
+    <app-header></app-header>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
