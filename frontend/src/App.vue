6a513083a2daf56c7be07aec04276d649aa08cbf
ianatskiy <igor.anatsky@waveaccess.ru>
2025-08-08 00:35:07 +0300

    step-5.15: Добавлена проверка авторизации и загрузка данных

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <app-layout>
-    <router-view />
+    <router-view v-if="isLoaded" />
   </app-layout>
 </template>
 
 <script setup>
 import AppLayout from "@/layouts/AppLayout.vue";
+import { tokenService } from "@/services/token-service";
+import { useAuthStore } from "@/stores/auth";
+import { onMounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useDataStore } from "@/stores";
+
+const route = useRoute();
+const router = useRouter();
+const dataStore = useDataStore();
+const isLoaded = ref(false);
+
+const checkLoggedIn = async () => {
+  const authStore = useAuthStore();
+  const token = tokenService.get();
+  if (!token) {
+    isLoaded.value = true;
+    return;
+  }
+
+  try {
+    await authStore.whoAmI();
+    const { redirect } = route.query;
+    await router.push(redirect ? redirect : { name: "home" });
+  } catch (e) {
+    tokenService.remove();
+    console.error(e);
+  } finally {
+    isLoaded.value = true;
+  }
+};
+
+onMounted(() => {
+  checkLoggedIn();
+  dataStore.loadData();
+});
 </script>
 
 <style lang="scss">
